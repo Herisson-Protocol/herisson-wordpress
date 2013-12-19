517b718f918a6b422f7b0d7d081f63390e6edf50
Thibault <thibault@ultimate.localdomain>
2013-12-19 21:17:14 +0100

    metadata

--- tools/generate_pot.sh
+++ tools/generate_pot.sh
@@ -1,2 +1,4 @@
-xgettext -k__ -k_e --language=PHP -o languages/en_US_new.pot admin/* herisson.php includes/*.php templates/default_templates/*
+#!/bin/bash
+
+xgettext -k__ -k_e --language=PHP -o ../src/languages/en_US_new.pot ../src/admin/* ../src/herisson.php ../src/includes/*.php ../src/templates/default_templates/*
 
