d379a27a0d623ea9aa71e64c657a6250b7dbe791
Thibault <thibault@ultimate.localdomain>
2013-12-19 20:24:50 +0100

    Moving all the source code into src/

--- install/install.sh
+++
@@ -1,3 +0,0 @@
-cat drop_db.sql | sed 's/#PREFIX#/wp_/g' | mysql -uroot -ptoto wordpress
-cat init_db.sql | sed 's/#PREFIX#/wp_/g' | mysql -uroot -ptoto wordpress
-cat init_data.sql | sed 's/#PREFIX#/wp_/g' | mysql -uroot -ptoto wordpress
