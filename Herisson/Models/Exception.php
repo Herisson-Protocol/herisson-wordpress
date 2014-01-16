57d1cbfac7e812285eef755bc8b57c9856808a39
Wilkins <thibault@taillandier.name>
2014-01-16 22:07:01 +0100

    Cleanup of Models classes

---
+++ Herisson/Models/Exception.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * HerissonModelException
+ * 
+ * PHP Version 5.3
+ * 
+ * @category Models
+ * @package  Herisson
+ * @author   Thibault Taillandier <thibault@taillandier.name>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPL v3
+ * @link     None
+ */
+
+/**
+ * HerissonModelException
+ *
+ * @category Models
+ * @package  Herisson
+ * @author   Thibault Taillandier <thibault@taillandier.name>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.txt GPL v3
+ * @link     None
+ */
+class HerissonModelException extends Exception
+{
+}
